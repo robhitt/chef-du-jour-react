218f66015070d8dc184b0ef5cbc7fe1ef1e4fde7
Yomi <alajide@gmail.com>
2017-02-17 16:18:46 -0500

    removed all console.logs

--- src/reducers/dining-experience-reducer.js
+++ src/reducers/dining-experience-reducer.js
@@ -1,7 +1,6 @@
 export default function diningExperiencesReducer(state = [], action){
   switch(action.type){
     case "CREATE_DINING_EXPERIENCE":
-    console.log(action.payload)
       return [...state, action.payload]
     case "FETCH_DINING_EXPERIENCES":
         return action.payload.data
--- src/reducers/my-dining-experiences-reducer.js
+++ src/reducers/my-dining-experiences-reducer.js
@@ -1,7 +1,6 @@
 export default function myDiningExperiencesReducer(state = [], action){
   switch(action.type){
     case "FETCH_MY_DINING_EXPERIENCES":
-    console.log(action.payload)
       return action.payload.data
     default:
     return state
