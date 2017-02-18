a55c6af43fd54343747c334cebfd791f9cd063ef
Yomi <alajide@gmail.com>
2017-02-17 19:50:39 -0500

    add semantic ui experimental cards

--- src/components/DiningExperienceIndex.js
+++ src/components/DiningExperienceIndex.js
@@ -19,24 +19,32 @@ import {Link} from 'react-router'
     const myDiningExperiences= this.props.myDiningExperiences
     return(
      <div>
-     <li><Link to='/user/create-dining-experience'><span className="glyphicon glyphicon-cutlery"/>Create a Dining Experience</Link></li>
-      <h2>Your Bookable Dining Experiences</h2>
-        <div className="row">
-        {myDiningExperiences.map((my_dining_experience) => {
-          return(
-            <div key={my_dining_experience.id}>
-                  <h5>{ my_dining_experience.title }</h5>
-                    <p>{my_dining_experience.description}</p><br/>
-            </div>
-          )
-        }
-      )}
-     </div>
-    </div>
+       <Link to='/user/create-dining-experience' className="btn btn-default"><span className="glyphicon glyphicon-cutlery"/> Create a Dining Experience</Link>
+       <div className="well well-sm"><span className="badge">{myDiningExperiences.length}</span> Bookable Dining Experiences</div>
+       <div className="ui cards">
+         {myDiningExperiences.map((my_dining_experience) => {
+           return(
 
+             <div className="card">
+               <div className="content">
+                 <div className="header" key={my_dining_experience.id}>{ my_dining_experience.title }</div>
+                 <div className="description">{my_dining_experience.description}</div>
+               </div>
+               <div className="ui bottom attached btn btn-default">
+                 Edit Experience
+               </div>
+             </div>
+           )
+         }
+         )}
+       </div>
+
+             </div>
    )
   }
 }
+
+
 
 function mapDispatchToProps(dispatch) {
   // return {
