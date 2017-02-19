7bca8b56d17b5881b9fab146a5e241b2cd4f1fb7
Yomi <alajide@gmail.com>
2017-02-19 17:01:53 -0500

    merge conflict

--- src/components/UserShow.js
+++ src/components/UserShow.js
@@ -5,9 +5,9 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router'
 
 class UserShow extends Component {
-  // constructor(props) {
-  //     super(props)
-  // }
+  constructor(props) {
+      super(props)
+  }
 
   componentDidMount(props){
     this.props.showUser(this.props.routeParams.id)
@@ -34,103 +34,95 @@ class UserShow extends Component {
               Last Name:
             </div>
             <div className="col-sm-10">
-              {this.props.users.last_name}
-            </div>
+              
           </div>
-
-          <div className="row">
-            <div className="col-sm-2">
-              Phone Number:
-            </div>
-            <div className="col-sm-10">
-              {this.props.users.phone_number}
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-sm-2">
-              Email:
-            </div>
-            <div className="col-sm-10">
-              {this.props.users.email}
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-sm-2">
-              Street 1:
-            </div>
-            <div className="col-sm-10">
-              {this.props.users.street1}
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-sm-2">
-              Street 2:
-            </div>
-            <div className="col-sm-10">
-              {this.props.users.street2}
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-sm-2">
-              City:
-            </div>
-            <div className="col-sm-10">
-              {this.props.users.city}
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-sm-2">
-              State:
-            </div>
-            <div className="col-sm-10">
-              {this.props.users.state}
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-sm-2">
-              Zip Code:
-            </div>
-            <div className="col-sm-10">
-              {this.props.users.zipcode}
-            </div>
-          </div>
-
-
-          <div className="row">
-            <div className="col-sm-2">
-              Bio:
-            </div>
-            <div className="col-sm-10">
-              {this.props.users.chef_biography}
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-sm-2">
-              Instagram
-            </div>
-            <div className="col-sm-10">
-              {this.props.users.instagram}
-            </div>
-          </div>
-
-          <p> </p>
-          <span className="button-seperator">
-            <Link to={"/user_edit"}><button type="button" className="btn btn-outline-primary btn-lg active">Edit Profile</button></Link>
-          </span>
-
-          <span className="button-seperator">
-            <Link to={"/user_edit"}><button type="button" className="btn btn-outline-primary btn-lg active">Delete Profile</button></Link>
-          </span>
-
         </div>
 
+        <div className="row">
+          <div className="col-sm-2">
+            Phone Number:
+          </div>
+          <div className="col-sm-10">
+            {this.props.users.phone_number}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-2">
+            Email:
+          </div>
+          <div className="col-sm-10">
+            {this.props.users.email}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-2">
+            Street 1:
+          </div>
+          <div className="col-sm-10">
+            {this.props.users.street1}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-2">
+            Street 2:
+          </div>
+          <div className="col-sm-10">
+            {this.props.users.street2}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-2">
+            City:
+          </div>
+          <div className="col-sm-10">
+            {this.props.users.city}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-2">
+            State:
+          </div>
+          <div className="col-sm-10">
+            {this.props.users.state}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-2">
+            Zip Code:
+          </div>
+          <div className="col-sm-10">
+            {this.props.users.zipcode}
+          </div>
+        </div>
+
+
+        <div className="row">
+          <div className="col-sm-2">
+            Bio:
+          </div>
+          <div className="col-sm-10">
+            {this.props.users.chef_biography}
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-2">
+            Instagram
+          </div>
+          <div className="col-sm-10">
+            {this.props.users.instagram}
+          </div>
+        </div>
+
+
+        </div>
+        <Link to={"/user_edit"}>Edit</Link>
         {this.props.children}
       </div>
 
