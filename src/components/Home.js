57a89c4bac6b3b50b3f576f3e3219e11396df5de
Rob Hitt <robhitt@gmail.com>
2017-05-01 09:48:25 -0400

    made landing page responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,6 @@ import {Link} from 'react-router'
 
 export default class Home extends Component {
 
-// Trying changing href below to Link to so jumbotron will work
-
 render(){
   return(
     <div className="background-photo">
@@ -12,11 +10,11 @@ render(){
 
         <div className="jumbotron" style={{paddingTop: "30px", paddingBottom: "30px", borderRadius: "15px"}}>
           <div className="border-space">
-            <img src="http://i65.tinypic.com/11vgolt.png" />
+            <img src="http://i65.tinypic.com/11vgolt.png" width="100%"/>
             <p>Welcome to Chef Du Jour!  Need a chef? We're your answer, anytime, anywhere.</p>
-            <p><Link to={"/signup"} className="btn btn-primary btn-lg button-color-scheme" role="button">Create An Account</Link>
+            <p><Link to={"/signup"} className="btn btn-primary btn-lg button-color-scheme margin-bot-5px" role="button">Create An Account</Link>
             <span> </span>
-            <Link to={"/signin"} className="btn btn-primary btn-lg button-color-scheme" role="button">Log In</Link></p>
+            <Link to={"/signin"} className="btn btn-primary btn-lg button-color-scheme margin-bot-5px" role="button">Log In</Link></p>
 
           </div>
         </div>
@@ -24,7 +22,4 @@ render(){
     </div>
       )
     }
-
 }
-
-// Link to={"/signup"}><span className="glyphicon glyphicon-user"/>
